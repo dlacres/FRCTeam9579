7c22e9c800a57fae56646060b86b48c560f2c7d5
dlacres <dlacres@gmail.com>
2024-02-08 09:37:11 -0600

    Create Limit.java

---
+++ Limit.java
@@ -0,0 +1,31 @@
+public class Limit {
+    private double max = 0;
+    private double min = 0;
+
+    public Limit(double max, double min) {
+        this.max = max;
+        this.min = min;
+    }
+
+    public Limit(double max) {
+        this.max = max;
+        this.min = -max;
+    }
+
+    double calculate(double in) {
+        double out;
+
+        out = in;
+        if (in > this.max) out = this.max;
+        if (in < this.min) out = this.min;
+        return (out);
+    }
+
+    void setMax(double max) {
+        this.max = max;
+    }
+
+    void setMin(double min) {
+        this.min = min;
+    }
+}
